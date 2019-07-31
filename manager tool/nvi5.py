4999ae1b821e3c7c973f9e28c953e5171bcc893e
gisempire <gisempire@foxmail.com>
2019-07-31 15:50:57 +0800

    Update nvi5.py

--- manager tool/nvi5.py
+++ manager tool/nvi5.py
@@ -1,12 +1,25 @@
- 
 import re
 import os
-
 import time
 
-#5 second run cmd NVSMI
+
 while 1:
     f = os.popen('C:/Program Files/NVIDIA Corporation/NVSMI/nvidia-smi.exe', 'r')
-    s = f.read()
-    print(s)
-    time.sleep(5)
+    s1 = f.read()
+
+    h1=s1.find('+')
+    strtime=s1[:h1]
+    print(strtime)
+    
+    h2=s1.find('N/A')
+    s1=s1[h2+6:]
+    h3=s1.find('+')
+    strinfo=s1[:h3]
+    print(strinfo)
+
+    with open("D:/temp/nvi5.txt","a") as texta:
+        texta.write(strtime)
+        texta.write(strinfo)
+    
+    time.sleep(10)
+
